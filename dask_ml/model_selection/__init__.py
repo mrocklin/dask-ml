f9e9707b2d2e62e3b7664bfeb3b0b0eb425f6593
Scott Sievert <github@stsievert.com>
2018-06-21 11:00:12 -0500

    ENH: initial implementation of Hyperband

--- dask_ml/model_selection/__init__.py
+++ dask_ml/model_selection/__init__.py
@@ -5,6 +5,7 @@ on the underlying estimators being used.
 """
 from dask_searchcv.model_selection import GridSearchCV, RandomizedSearchCV  # noqa
 from ._split import ShuffleSplit, train_test_split
+from ._hyperband import Hyperband
 
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     'RandomizedSearchCV',
     'ShuffleSplit',
     'train_test_split',
+    'Hyperband',
 ]
